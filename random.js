7242b2f54ea6c049b23bb3ebf097301b78353361
saintjack <saintjack@Nicholass-MacBook-Air.local>
2018-06-06 14:01:38 -0400

    Addedd parameters and arguesments to existing function. As well as added a new function to determine area

--- random.js
+++ random.js
@@ -1,1 +1,16 @@
-var randomNumber = Math.floor( Math.random() * 6 ) + 1; 
+// Random Number Generator
+function getRandomNumber(upper) {
+  var randomNumber = Math.floor( Math.random() * upper ) + 1;
+  return randomNumber;
+}
+console.log(getRandomNumber(10));
+console.log(getRandomNumber(100));
+
+
+// Get the area
+function getArea(width, length) {
+  var area = width * length;
+  return area;
+}
+
+console.log(getArea(10, 300))
